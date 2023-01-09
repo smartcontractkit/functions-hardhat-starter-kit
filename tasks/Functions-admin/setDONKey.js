d3ab0320c9ef6157b3a95fb4b214fdaaf65d41b1
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2023-01-09 13:03:03 -0600

    added higher goerli gas limit to all commands

--- tasks/Functions-admin/setDONKey.js
+++ tasks/Functions-admin/setDONKey.js
@@ -6,11 +6,20 @@ task('functions-set-don-key', 'Sets the DON public key in the Functions oracle c
       throw Error('This command cannot be used on a local development chain.  Specify a valid network.')
     }
 
+    let overrides = undefined
+    if (network.name === 'goerli') {
+      overrides = {
+        // be careful, this may drain your balance quickly
+        maxPriorityFeePerGas: ethers.utils.parseUnits("50", "gwei"),
+        maxFeePerGas: ethers.utils.parseUnits("50", "gwei"),
+      }
+    }
+
     const oracleFactory = await ethers.getContractFactory('FunctionsOracle')
     const oracle = oracleFactory.attach(networkConfig[network.name]['functionsOracle'])
 
     console.log(`Setting oracle OCR config for oracle ${networkConfig[network.name]['functionsOracle']}`)
-    const setTx = await oracle.setDONPublicKey('0x' + networkConfig[network.name]['functionsPublicKey'])
+    const setTx = await oracle.setDONPublicKey('0x' + networkConfig[network.name]['functionsPublicKey'], overrides)
 
     console.log(`Waiting ${VERIFICATION_BLOCK_CONFIRMATIONS} blocks for transaction ${setTx.hash} to be confirmed...`)
     await setTx.wait(VERIFICATION_BLOCK_CONFIRMATIONS)
