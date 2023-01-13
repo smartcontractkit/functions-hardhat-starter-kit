ffa398db13f4dee5375ddcfa98ebb4024c90b815
Morgan Kuphal <87319522+KuphJr@users.noreply.github.com>
2023-01-13 14:00:44 -0600

    Update network-config.js

--- network-config.js
+++ network-config.js
@@ -4,8 +4,8 @@ require("dotenv").config()
 const getLinkEthPriceFeed = () => {
     if (process.env.MAINNET_RPC_URL) return "0xdc530d9457755926550b59e8eccdae7624181557"
     if (process.env.POLYGON_MAINNET_RPC_URL) return "0xb77fa460604b9c6435a235d057f7d319ac83cb53"
-    if (process.env.GOERLI_RPC_URL) return "0xb4c4a493AB6356497713A78FFA6c60FB53517c63"
     if (process.env.MUMBAI_RPC_URL) return "0x12162c3E810393dEC01362aBf156D7ecf6159528"
+    if (process.env.GOERLI_RPC_URL) return "0xb4c4a493AB6356497713A78FFA6c60FB53517c63"
     if (process.env.SEPOLIA_RPC_URL) return "NOT_AVAILABLE_YET"
 }
 
