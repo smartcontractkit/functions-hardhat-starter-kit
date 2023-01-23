469b484c2667f794de7dd9dc72dc26f3b59a6671
aelmanaa <aelmanaa@gmail.com>
2023-01-23 13:54:47 +0100

    tutorial 3

--- Functions-request-config.js
+++ Functions-request-config.js
@@ -24,29 +24,18 @@ const ReturnType = {
 
 // Configure the request by setting the fields below
 const requestConfig = {
-  // location of source code (only Inline is currently supported)
+  // location of source code (only Inline is curently supported)
   codeLocation: Location.Inline,
   // location of secrets (Inline or Remote)
   secretsLocation: Location.Inline,
   // code language (only JavaScript is currently supported)
   codeLanguage: CodeLanguage.JavaScript,
   // string containing the source code to be executed
-  source: fs.readFileSync("./calculation-example.js").toString(),
-  //source: fs.readFileSync('./API-request-example.js').toString(),
-  // secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey). The secrets object can only contain string values.
-  secrets: { apiKey: process.env.COINMARKETCAP_API_KEY ?? '' },
-  // ETH wallet key used to sign secrets so they cannot be accessed by a 3rd party
-  walletPrivateKey: process.env["PRIVATE_KEY"],
-  // args (string only array) can be accessed within the source code with `args[index]` (ie: args[0]).
-  args: ["1", "bitcoin", "btc-bitcoin", "btc", "1000000", "450"],
+  source: fs.readFileSync("./Functions-request-source.js").toString(),
+  // args can be accessed within the source code with `args[index]` (ie: args[0])
+  args: ["ETH", "USD"],
   // expected type of the returned value
-  expectedReturnType: ReturnType.uint256,
-  // Redundant URLs which point to encrypted off-chain secrets
-  secretsURLs: [],
-  // Default offchain secrets object used by the `functions-build-offchain-secrets` command
-  globalOffchainSecrets: {},
-  // Per-node offchain secrets objects used by the `functions-build-offchain-secrets` command
-  perNodeOffchainSecrets: [],
+  expectedReturnType: ReturnType.string,
 }
 
 module.exports = requestConfig
