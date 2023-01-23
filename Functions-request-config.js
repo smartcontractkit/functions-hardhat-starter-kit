1afbf19d2fd92ae86834f124c7e9c1d9686099c4
aelmanaa <aelmanaa@gmail.com>
2023-01-23 13:54:47 +0100

    tutorial 5

--- Functions-request-config.js
+++ Functions-request-config.js
@@ -24,25 +24,20 @@ const ReturnType = {
 
 // Configure the request by setting the fields below
 const requestConfig = {
-  // Location of source code (only Inline is currently supported)
+  // location of source code (only Inline is curently supported)
   codeLocation: Location.Inline,
-  // Code language (only JavaScript is currently supported)
+  // code language (only JavaScript is currently supported)
   codeLanguage: CodeLanguage.JavaScript,
-  // String containing the source code to be executed
-  source: fs.readFileSync("./calculation-example.js").toString(),
-  //source: fs.readFileSync('./API-request-example.js').toString(),
-  // Secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey). The secrets object can only contain string values.
-  //secrets: { apiKey: process.env.COINMARKETCAP_API_KEY ?? "" },
-  // Per-node secrets objects assigned to each DON member. When using per-node secrets, nodes can only use secrets which they have been assigned.
-  perNodeSecrets: [],
+  // string containing the source code to be executed
+  source: fs.readFileSync("./Functions-request-source.js").toString(),
+  // secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey)
+  secrets: { apiKey: process.env.COINMARKETCAP_API_KEY ?? "" },
   // ETH wallet key used to sign secrets so they cannot be accessed by a 3rd party
   walletPrivateKey: process.env["PRIVATE_KEY"],
-  // Args (string only array) can be accessed within the source code with `args[index]` (ie: args[0]).
-  args: ["1", "bitcoin", "btc-bitcoin", "btc", "1000000", "450"],
-  // Expected type of the returned value
+  // args can be accessed within the source code with `args[index]` (ie: args[0])
+  args: ["1", "USD"],
+  // expected type of the returned value
   expectedReturnType: ReturnType.uint256,
-  // Redundant URLs which point to encrypted off-chain secrets
-  secretsURLs: [],
 }
 
 module.exports = requestConfig
