e28e55314b58099f54e7ceb39b9ca51ceb7ff3e7
aelmanaa <aelmanaa@gmail.com>
2023-01-23 13:54:47 +0100

    tutorial 1

--- Functions-request-config.js
+++ Functions-request-config.js
@@ -28,21 +28,12 @@ const requestConfig = {
   codeLocation: Location.Inline,
   // Code language (only JavaScript is currently supported)
   codeLanguage: CodeLanguage.JavaScript,
-  // String containing the source code to be executed
-  source: fs.readFileSync("./calculation-example.js").toString(),
-  //source: fs.readFileSync('./API-request-example.js').toString(),
-  // Secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey). The secrets object can only contain string values.
-  //secrets: { apiKey: process.env.COINMARKETCAP_API_KEY ?? "" },
-  // Per-node secrets objects assigned to each DON member. When using per-node secrets, nodes can only use secrets which they have been assigned.
-  perNodeSecrets: [],
-  // ETH wallet key used to sign secrets so they cannot be accessed by a 3rd party
-  walletPrivateKey: process.env["PRIVATE_KEY"],
-  // Args (string only array) can be accessed within the source code with `args[index]` (ie: args[0]).
-  args: ["1", "bitcoin", "btc-bitcoin", "btc", "1000000", "450"],
-  // Expected type of the returned value
+  // string containing the source code to be executed
+  source: fs.readFileSync("./Functions-request-source.js").toString(),
+  // args can be accessed within the source code with `args[index]` (ie: args[0])
+  args: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+  // expected type of the returned value
   expectedReturnType: ReturnType.uint256,
-  // Redundant URLs which point to encrypted off-chain secrets
-  secretsURLs: [],
 }
 
 module.exports = requestConfig
