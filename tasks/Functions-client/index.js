c03c3db005de8083a2e2d98251005503ac97af8a
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2023-01-25 09:35:16 -0600

    added (optional) to command parameter in readme

--- tasks/Functions-client/index.js
+++ tasks/Functions-client/index.js
@@ -4,12 +4,4 @@ exports.requestData = require("./request.js")
 exports.simulate = require("./simulate.js")
 exports.deploy = require("./deploy.js")
 exports.setOracleAddr = require("./setOracleAddr.js")
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 exports.buildOffchainSecrets = require("./buildOffchainSecrets.js")
->>>>>>> 7e919e36 (user-facing offchain secrets tooling)
-exports.buildRequestJSON = require("./buildRequestJSON.js")
-=======
-exports.buildOffchainSecrets = require("./buildOffchainSecrets.js")
->>>>>>> 179a39e3 (moving functions-build-request & contract changes to another PR)
