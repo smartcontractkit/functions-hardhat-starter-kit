7826ddfd33ea1b9675db711b92f87c9a008c38eb
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2023-02-08 14:10:18 -0600

    added Stripe example

--- Functions-request-config.js
+++ Functions-request-config.js
@@ -31,14 +31,14 @@ const requestConfig = {
   // code language (only JavaScript is currently supported)
   codeLanguage: CodeLanguage.JavaScript,
   // string containing the source code to be executed
-  source: fs.readFileSync("./Functions-request-source-calculation-example.js").toString(),
-  //source: fs.readFileSync('./Functions-request-source-API-example.js').toString(),
+  //source: fs.readFileSync("./Functions-request-source-calculation-example.js").toString(),
+  source: fs.readFileSync('./Functions-request-source-API-example.js').toString(),
   // secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey)
-  secrets: { apiKey: process.env.COINMARKETCAP_API_KEY },
+  secrets: { API_KEY: process.env.STRIPE_API_KEY },
   // ETH wallet key used to sign secrets so they cannot be accessed by a 3rd party
   walletPrivateKey: process.env["PRIVATE_KEY"],
   // args (string only array) can be accessed within the source code with `args[index]` (ie: args[0]).
-  args: ["1", "bitcoin", "btc-bitcoin", "btc", "1000000", "450"],
+  args: ["usd"],
   // expected type of the returned value
   expectedReturnType: ReturnType.uint256,
   // Redundant URLs which point to encrypted off-chain secrets
