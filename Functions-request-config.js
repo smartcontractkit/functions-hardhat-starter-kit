f1ebb371f38d71a112cd84e9c1f4c4b54fe141ab
Zubin Pratap <zubin.pratap@gmail.com>
2023-02-09 09:33:11 -0800

    Initial sample for twilio-spotify JS code added. Works on local functions simulator.
    * request config + source
    * smart contract to be updated next with some logic for calculating payment(?)
    
    This commit includes code pulled and rebased on top of df76cff.

--- Functions-request-config.js
+++ Functions-request-config.js
@@ -30,17 +30,17 @@ const requestConfig = {
   secretsLocation: Location.Inline,
   // code language (only JavaScript is currently supported)
   codeLanguage: CodeLanguage.JavaScript,
-  // string containing the source code to be executed
-  source: fs.readFileSync("./calculation-example.js").toString(),
-  //source: fs.readFileSync('./API-request-example.js').toString(),
+  // string containing the source code to be executed. Relative path used.
+  source: fs.readFileSync("samples/twilio-spotify/Twilio-Spotify-Functions-Source-Example.js").toString(),
   // secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey)
-  secrets: { apiKey: process.env.COINMARKETCAP_API_KEY },
+  secrets: { soundchartAppId: "soundcharts", soundchartApiKey: "soundcharts", twilioApiKey: process.env.TWILIO_API_KEY }, // TODO: use ENV VARS
   // ETH wallet key used to sign secrets so they cannot be accessed by a 3rd party
   walletPrivateKey: process.env["PRIVATE_KEY"],
   // args (string only array) can be accessed within the source code with `args[index]` (ie: args[0]).
-  args: ["1", "bitcoin", "btc-bitcoin", "btc", "1000000", "450"],
+  // args in sequence are: ArtistID, artistname,  lastMonthlyListenerCount, artist email
+  args: ["ca22091a-3c00-11e9-974f-549f35141000", "Zubin P", "14045928", "zubin.pratap+demos@smartcontract.com"], // TONES_AND_I // TODO @ Zubin fix email
   // expected type of the returned value
-  expectedReturnType: ReturnType.uint256,
+  expectedReturnType: ReturnType.string,
   // Redundant URLs which point to encrypted off-chain secrets
   secretsURLs: [],
   // Default offchain secrets object used by the `functions-build-offchain-secrets` command
@@ -48,5 +48,31 @@ const requestConfig = {
   // Per-node offchain secrets objects used by the `functions-build-offchain-secrets` command
   perNodeOffchainSecrets: [],
 }
+
+// {
+//   // location of source code (only Inline is currently supported)
+//   codeLocation: Location.Inline,
+//   // location of secrets (Inline or Remote)
+//   secretsLocation: Location.Inline,
+//   // code language (only JavaScript is currently supported)
+//   codeLanguage: CodeLanguage.JavaScript,
+//   // string containing the source code to be executed
+//   source: fs.readFileSync("./Functions-request-source-calculation-example.js").toString(),
+//   //source: fs.readFileSync('./Functions-request-source-API-example.js').toString(),
+//   // secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey)
+//   secrets: { apiKey: process.env.COINMARKETCAP_API_KEY },
+//   // ETH wallet key used to sign secrets so they cannot be accessed by a 3rd party
+//   walletPrivateKey: process.env["PRIVATE_KEY"],
+//   // args (string only array) can be accessed within the source code with `args[index]` (ie: args[0]).
+//   args: ["1", "bitcoin", "btc-bitcoin", "btc", "1000000", "450"],
+//   // expected type of the returned value
+//   expectedReturnType: ReturnType.uint256,
+//   // Redundant URLs which point to encrypted off-chain secrets
+//   secretsURLs: [],
+//   // Default offchain secrets object used by the `functions-build-offchain-secrets` command
+//   globalOffchainSecrets: {},
+//   // Per-node offchain secrets objects used by the `functions-build-offchain-secrets` command
+//   perNodeOffchainSecrets: [],
+// }
 
 module.exports = requestConfig
