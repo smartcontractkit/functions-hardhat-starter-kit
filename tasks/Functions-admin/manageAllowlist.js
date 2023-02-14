5fcf106a5d0628fa24450032573874ee2cc46b1b
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2023-02-14 17:39:24 -0600

    pretteir

--- tasks/Functions-admin/manageAllowlist.js
+++ tasks/Functions-admin/manageAllowlist.js
@@ -26,8 +26,12 @@ async function addOrRemove(action, taskArgs) {
   let addresses
   if (taskArgs.addresses) {
     addresses = taskArgs.addresses.split(",")
-    addresses = addresses.filter((a) => { return ethers.utils.isAddress(a)})
-    const invalidAddresses = addresses.filter((a) => { return !ethers.utils.isAddress(a)})
+    addresses = addresses.filter((a) => {
+      return ethers.utils.isAddress(a)
+    })
+    const invalidAddresses = addresses.filter((a) => {
+      return !ethers.utils.isAddress(a)
+    })
     console.log(`The following addresses are invalid and will be ignored: ${invalidAddresses}`)
   }
 
@@ -45,7 +49,7 @@ async function addOrRemove(action, taskArgs) {
       : await oracle.addAuthorizedSenders(addresses)
   } else {
     if (!addresses) {
-      throw Error('No addresses provided')
+      throw Error("No addresses provided")
     }
     console.log(`Removing addresses ${addresses} from oracle ${networkConfig[network.name]["functionsOracle"]}`)
     tx = overrides
@@ -59,12 +63,21 @@ async function addOrRemove(action, taskArgs) {
   console.log(`Allowlist updated for oracle ${oracle.address} on ${network.name}`)
 }
 
-task("functions-add-senders", "Add wallets to allowlist in the Oracle contract.  In order to add users from allowlist.csv, copy the CSV file into the root directory and do not set the addresses parameter.")
-  .addOptionalParam("addresses", "Comma-separated list of addresses.  If this is not provided, addresses will be pulled from the allowlist CSV file")
+task(
+  "functions-add-senders",
+  "Add wallets to allowlist in the Oracle contract.  In order to add users from allowlist.csv, copy the CSV file into the root directory and do not set the addresses parameter."
+)
+  .addOptionalParam(
+    "addresses",
+    "Comma-separated list of addresses.  If this is not provided, addresses will be pulled from the allowlist CSV file"
+  )
   .addOptionalParam("filename", "Name of the allowlist CSV file (defaults to allowlist.csv)")
-  .addOptionalParam("eventcodes", "Comma-separated list of valid event code that must be provided by the user to be added")
+  .addOptionalParam(
+    "eventcodes",
+    "Comma-separated list of valid event code that must be provided by the user to be added"
+  )
   .setAction(async (taskArgs) => {
-      await addOrRemove(Action.Add, taskArgs)
+    await addOrRemove(Action.Add, taskArgs)
   })
 
 task("functions-remove-senders", "Remove wallets from allowlist in the Oracle contract")
@@ -81,7 +94,7 @@ const addFromAllowlist = async (taskArgs, oracle, overrides) => {
   const addressesToAdd = filterAddressesToAdd(currentAllowlist, allowlistData.validUsers)
 
   if (addressesToAdd.length === 0) {
-    console.log('No new valid addresses to add')
+    console.log("No new valid addresses to add")
   } else {
     console.log(`Adding ${addressesToAdd.length} new addresses`)
 
@@ -89,17 +102,17 @@ const addFromAllowlist = async (taskArgs, oracle, overrides) => {
     let tx
     for (let i = 0; i < addressesToAdd.length; i += addressChunkSize) {
       const addressChunk = addressesToAdd.slice(i, Math.min(i + addressChunkSize, addressesToAdd.length))
-      
-      console.log('Adding the following addresses:')
+
+      console.log("Adding the following addresses:")
       console.log(addressChunk)
-  
+
       tx = overrides
         ? await oracle.addAuthorizedSenders(addressChunk, overrides)
         : await oracle.addAuthorizedSenders(addressChunk)
-  
+
       await tx.wait(1)
     }
-  
+
     tx.wait(VERIFICATION_BLOCK_CONFIRMATIONS - 1)
   }
 
@@ -109,9 +122,9 @@ const addFromAllowlist = async (taskArgs, oracle, overrides) => {
 }
 
 const getDataFromAllowlist = (requiredEventCodes, allowlistFileName) => {
-  const allowList = fs.readFileSync(allowlistFileName ?? './allowlist.csv').toString()
+  const allowList = fs.readFileSync(allowlistFileName ?? "./allowlist.csv").toString()
 
-  const allowListLines = allowList.split('\n')
+  const allowListLines = allowList.split("\n")
   // Ignore the first line which contains column titles
   const titles = allowListLines[0]
   const lines = allowListLines.slice(1)
@@ -123,15 +136,15 @@ const getDataFromAllowlist = (requiredEventCodes, allowlistFileName) => {
 
   // Split address from email and place into an object
   for (const line of lines) {
-    const columns = line.split(',')
+    const columns = line.split(",")
 
     // Remove extraneous spaces from user's typed entries
     const user = {
       approved: columns[0],
-      walletAddress: columns[1].replace(/\s/g, ''),
-      email: columns[2].replace(/\s/g, ''),
+      walletAddress: columns[1].replace(/\s/g, ""),
+      email: columns[2].replace(/\s/g, ""),
       agreedToTOS: columns[3],
-      eventCode: columns[4].replace(/\s/g, ''),
+      eventCode: columns[4].replace(/\s/g, ""),
       utm_medium: columns[5],
       utm_source: columns[6],
       utm_content: columns[7],
@@ -145,23 +158,21 @@ const getDataFromAllowlist = (requiredEventCodes, allowlistFileName) => {
     allUsers.push(user)
     // Check that user is valid
     if (!ethers.utils.isAddress(user.walletAddress)) {
-      user.notes = 'INVALID WALLET ADDRESS ' + user.notes
+      user.notes = "INVALID WALLET ADDRESS " + user.notes
       invalidUsers.push(user)
       continue
     }
-    if (user.agreedToTOS.toLowerCase() !== 'true') {
-      user.notes = 'DID NOT AGREE TO TOS' + user.notes
+    if (user.agreedToTOS.toLowerCase() !== "true") {
+      user.notes = "DID NOT AGREE TO TOS" + user.notes
       invalidUsers.push(user)
       continue
     }
     if (
       requiredEventCodes &&
-      (
-        user.eventCode.toLowerCase() === '' ||
-        !requiredEventCodes.toLowerCase().split(',').includes(user.eventCode.toLowerCase())
-      )
+      (user.eventCode.toLowerCase() === "" ||
+        !requiredEventCodes.toLowerCase().split(",").includes(user.eventCode.toLowerCase()))
     ) {
-      user.notes = 'INVALID EVENT CODE' + user.notes
+      user.notes = "INVALID EVENT CODE" + user.notes
       invalidUsers.push(user)
       continue
     }
@@ -202,30 +213,30 @@ const generateUpdatedAllowlistCsv = (updatedOnChainAllowlist, allowlistData) => 
     onChainAllowlistMap[address.toLowerCase()] = true
   })
 
-  const lines = [ allowlistData.titles ]
+  const lines = [allowlistData.titles]
 
   for (let i = 0; i < allowlistData.allUsers.length; i++) {
     if (onChainAllowlistMap[allowlistData.allUsers[i].walletAddress.toLowerCase()]) {
-      allowlistData.allUsers[i].approved = 'Yes'
+      allowlistData.allUsers[i].approved = "Yes"
     } else {
-      allowlistData.allUsers[i].approved = ''
+      allowlistData.allUsers[i].approved = ""
     }
 
     lines.push(createEntryLine(allowlistData.allUsers[i]))
   }
 
-  const updatedCsvText = lines.join('\n')
-  fs.writeFileSync('./updatedAllowlist.csv', updatedCsvText)
+  const updatedCsvText = lines.join("\n")
+  fs.writeFileSync("./updatedAllowlist.csv", updatedCsvText)
 
-  const invalidLines = [ allowlistData.titles ]
+  const invalidLines = [allowlistData.titles]
 
   for (const invalidUser of allowlistData.invalidUsers) {
     invalidLines.push(createEntryLine(invalidUser))
   }
 
-  const invalidCsvText = invalidLines.join('\n')
+  const invalidCsvText = invalidLines.join("\n")
 
-  fs.writeFileSync('./invalidUsers.csv', invalidCsvText)
+  fs.writeFileSync("./invalidUsers.csv", invalidCsvText)
 }
 
 const createEntryLine = (user) => {
@@ -242,7 +253,7 @@ const createEntryLine = (user) => {
     utm_campaign,
     submittedAt,
     token,
-    notes
+    notes,
   } = user
 
   return `${approved},${walletAddress},${email},${agreedToTOS},${eventCode},${utm_medium},${utm_source},${utm_content},${utm_term},${utm_campaign},${submittedAt},${token},${notes}`
