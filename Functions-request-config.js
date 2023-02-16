ba803c5af6885e00f96e2b38d1f4e06c3ada80b4
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2023-02-16 15:49:36 -0600

    added automated heartbeat test

--- Functions-request-config.js
+++ Functions-request-config.js
@@ -27,12 +27,11 @@ const requestConfig = {
   // location of source code (only Inline is currently supported)
   codeLocation: Location.Inline,
   // location of secrets (Inline or Remote)
-  secretsLocation: Location.Inline,
+  secretsLocation: Location.Remote,
   // code language (only JavaScript is currently supported)
   codeLanguage: CodeLanguage.JavaScript,
   // string containing the source code to be executed
-  source: fs.readFileSync("./calculation-example.js").toString(),
-  //source: fs.readFileSync('./API-request-example.js').toString(),
+  source: fs.readFileSync("./heartbeat-test.js").toString(),
   // secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey)
   secrets: { apiKey: process.env.COINMARKETCAP_API_KEY },
   // ETH wallet key used to sign secrets so they cannot be accessed by a 3rd party
@@ -42,11 +41,21 @@ const requestConfig = {
   // expected type of the returned value
   expectedReturnType: ReturnType.uint256,
   // Redundant URLs which point to encrypted off-chain secrets
-  secretsURLs: [],
+  secretsURLs: [
+    'https://raw.githubusercontent.com/KuphJr/off-chain-secrets/main/mumbai-heartbeat-test-secrets.json'
+    //'https://raw.githubusercontent.com/KuphJr/off-chain-secrets/main/sepolia-heartbeat-test-secrets.json'
+  ],
   // Default offchain secrets object used by the `functions-build-offchain-secrets` command
-  globalOffchainSecrets: {},
+  globalOffchainSecrets: {
+    test: 100
+  },
   // Per-node offchain secrets objects used by the `functions-build-offchain-secrets` command
-  perNodeOffchainSecrets: [],
+  perNodeOffchainSecrets: [
+    { test: 1000 },
+    { test: 2000 },
+    { test: 3000 },
+    { test: 4000 },
+  ],
 }
 
 module.exports = requestConfig
