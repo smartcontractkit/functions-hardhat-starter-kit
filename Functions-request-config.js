664a772e3964256964fd6e91d9b324f8b286df1d
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2023-02-20 18:08:20 -0600

    updated README & Functions-request-config.js to indicate only strings can be used in the secrets object

--- Functions-request-config.js
+++ Functions-request-config.js
@@ -33,7 +33,7 @@ const requestConfig = {
   // string containing the source code to be executed
   source: fs.readFileSync("./calculation-example.js").toString(),
   //source: fs.readFileSync('./API-request-example.js').toString(),
-  // secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey)
+  // secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey). The secrets object can only contain string values.
   secrets: { apiKey: process.env.COINMARKETCAP_API_KEY },
   // ETH wallet key used to sign secrets so they cannot be accessed by a 3rd party
   walletPrivateKey: process.env["PRIVATE_KEY"],
