9395b87cda8c14ca5e439153ba9c5cc3e4557d6d
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2023-02-22 16:52:18 -0600

    addressed feedback

--- tasks/Functions-billing/timeoutRequest.js
+++ tasks/Functions-billing/timeoutRequest.js
@@ -1,7 +1,7 @@
-const { networkConfig } = require("../../network-config")
+const { networkConfig, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../../network-config")
 
 task("functions-timeout-requests", "Times out expired requests")
-  .addParam("requestids", "1 or more request IDs to timeout")
+  .addParam("requestids", "1 or more request IDs to timeout separated by commas")
   .setAction(async (taskArgs) => {
     if (network.name === "hardhat") {
       throw Error(
@@ -9,16 +9,30 @@ task("functions-timeout-requests", "Times out expired requests")
       )
     }
 
+    const requestIdsToTimeout = taskArgs.requestids.split(",")
+
     console.log(
-      `Timing out requests ${taskArgs.requestids} on ${taskArgs.network}`
+      `Timing out requests ${requestIdsToTimeout} on ${network.name}`
     )
 
     const RegistryFactory = await ethers.getContractFactory("FunctionsBillingRegistry")
     const registry = await RegistryFactory.attach(networkConfig[network.name]["functionsBillingRegistryProxy"])
 
-    await registry.timeoutRequests(taskArgs.requestids)
+    // Listen for timed-out request events
+    registry.on("RequestTimedOut", (timedOutRequestId) => {
+      // Filter in case multiple users attempt to time out requests simultaneously
+      if (requestIdsToTimeout.includes(timedOutRequestId)) {
+        console.log(`Request ${timedOutRequestId} successfully timed out`)
+      }
+    })
 
-    console.log(
-      `Requests ${taskArgs.requestids} are no longer pending or have been timed out`
-    )
+    const timeoutTx = await registry.timeoutRequests(requestIdsToTimeout, { gasLimit: 10_000_000 })
+
+    console.log(`Waiting ${VERIFICATION_BLOCK_CONFIRMATIONS} blocks for transaction ${timeoutTx.hash} to be confirmed...`)
+    await timeoutTx.wait(VERIFICATION_BLOCK_CONFIRMATIONS)
+
+    // Close the event listener
+    await registry.removeAllListeners()
+    
+    console.log("\nTimeout requests transaction complete")
   })
