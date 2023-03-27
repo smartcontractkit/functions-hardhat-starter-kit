ce68cf75c85cde66ebc04f99672bd8d70e501369
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2023-03-27 08:56:29 -0500

    updated FunctionsSandboxLibrary

--- FunctionsSandboxLibrary/Log.js
+++ FunctionsSandboxLibrary/Log.js
@@ -1,76 +1,67 @@
-"use strict"
-Object.defineProperty(exports, "__esModule", { value: true })
-exports.secretsRedactorFactory = exports.Log = void 0
-class Log {}
-exports.Log = Log
-Log.error = (message, requestId) =>
-  console.log(
-    JSON.stringify({
-      logLevel: "error",
-      timestamp: Date.now(),
-      message,
-      requestId,
-    })
-  )
-Log.warn = (message, requestId) =>
-  console.log(
-    JSON.stringify({
-      logLevel: "warn",
-      timestamp: Date.now(),
-      message,
-      requestId,
-    })
-  )
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.secretsRedactorFactory = exports.Log = void 0;
+class Log {
+}
+exports.Log = Log;
+Log.error = (message, requestId) => console.log(JSON.stringify({
+    logLevel: 'error',
+    timestamp: Date.now(),
+    message,
+    requestId,
+}));
+Log.warn = (message, requestId) => console.log(JSON.stringify({
+    logLevel: 'warn',
+    timestamp: Date.now(),
+    message,
+    requestId,
+}));
 Log.info = (message, requestId) => {
-  if (process.env["LOG_LEVEL"] && process.env["LOG_LEVEL"]?.toLowerCase() !== "false") {
-    console.log(
-      JSON.stringify({
-        logLevel: "info",
-        timestamp: Date.now(),
-        message,
-        requestId,
-      })
-    )
-  }
-}
-Log.debug = (message, requestId) => {
-  if (process.env["LOG_LEVEL"]?.toLowerCase() === "debug" || process.env["LOG_LEVEL"]?.toLowerCase() === "trace") {
-    console.log(
-      JSON.stringify({
-        logLevel: "debug",
-        timestamp: Date.now(),
-        message,
-        requestId,
-      })
-    )
-  }
-}
-Log.trace = (message, requestId) => {
-  if (process.env["LOG_LEVEL"]?.toLowerCase() === "trace") {
-    console.log(
-      JSON.stringify({
-        logLevel: "trace",
-        timestamp: Date.now(),
-        message,
-        requestId,
-      })
-    )
-  }
-}
-const secretsRedactorFactory = (secrets) => {
-  const allSecretsToRedact = Array.from(new Set(getSecretsToRedact(secrets))).filter((s) => s !== "<REDACTED SECRET>")
-  return function secretsRedactor(stringToRedact) {
-    let redactedString = stringToRedact
-    for (const secret of allSecretsToRedact) {
-      redactedString = redactedString.split(secret).join("<REDACTED SECRET>").slice(0, 2048)
+    if (process.env['LOG_LEVEL'] &&
+        process.env['LOG_LEVEL']?.toLowerCase() !== 'false') {
+        console.log(JSON.stringify({
+            logLevel: 'info',
+            timestamp: Date.now(),
+            message,
+            requestId,
+        }));
     }
-    // Curtail the URL length to prevent excess logging
-    return redactedString
-  }
-}
-exports.secretsRedactorFactory = secretsRedactorFactory
+};
+Log.debug = (message, requestId) => {
+    if (process.env['LOG_LEVEL']?.toLowerCase() === 'debug' ||
+        process.env['LOG_LEVEL']?.toLowerCase() === 'trace') {
+        console.log(JSON.stringify({
+            logLevel: 'debug',
+            timestamp: Date.now(),
+            message,
+            requestId,
+        }));
+    }
+};
+Log.trace = (message, requestId) => {
+    if (process.env['LOG_LEVEL']?.toLowerCase() === 'trace') {
+        console.log(JSON.stringify({
+            logLevel: 'trace',
+            timestamp: Date.now(),
+            message,
+            requestId,
+        }));
+    }
+};
+const secretsRedactorFactory = (secrets) => {
+    const allSecretsToRedact = Array.from(new Set(getSecretsToRedact(secrets))).filter((s) => s !== '<REDACTED SECRET>');
+    return function secretsRedactor(stringToRedact) {
+        let redactedString = stringToRedact;
+        for (const secret of allSecretsToRedact) {
+            redactedString = redactedString.split(secret).join('<REDACTED SECRET>').slice(0, 2048);
+        }
+        // Curtail the URL length to prevent excess logging
+        return redactedString;
+    };
+};
+exports.secretsRedactorFactory = secretsRedactorFactory;
 const getSecretsToRedact = (secrets) => {
-  const secretsKeys = Object.keys(secrets)
-  const secretsValues = Object.values(secrets)
-  return secretsKeys.concat(secretsValues)
-}
+    const secretsKeys = Object.keys(secrets);
+    const secretsValues = Object.values(secrets);
+    return secretsKeys.concat(secretsValues);
+};
--- FunctionsSandboxLibrary/buildRequest.js
+++ FunctionsSandboxLibrary/buildRequest.js
@@ -1,47 +1,17 @@
-"use strict"
-Object.defineProperty(exports, "__esModule", { value: true })
-exports.buildRequest = void 0
-const getRequestConfig_1 = require("./getRequestConfig")
-const encryptSecrets_1 = require("./encryptSecrets")
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.buildRequest = void 0;
+const getRequestConfig_1 = require("./getRequestConfig");
+const encryptSecrets_1 = require("./encryptSecrets");
 const buildRequest = async (unvalidatedConfig) => {
-  const config = (0, getRequestConfig_1.getRequestConfig)(unvalidatedConfig)
-  const request = { source: config.source }
-  if (
-    (config.secrets && Object.keys(config.secrets).length > 0) ||
-    (config.secretsURLs && config.secretsURLs.length > 0)
-  ) {
-    if (!config.DONPublicKey) {
-      throw Error(`DONPublicKey not in config`)
+    const config = (0, getRequestConfig_1.getRequestConfig)(unvalidatedConfig);
+    const request = { source: config.source };
+    if (config.secretsURLs && config.secretsURLs.length > 0) {
+        request.secrets = '0x' + (await (0, encryptSecrets_1.encrypt)(config.DONPublicKey, config.secretsURLs.join(' ')));
     }
-    if (config.secretsLocation === getRequestConfig_1.Location_.Inline) {
-      if (typeof config.secrets !== "object") {
-        throw Error("Unsupported inline secrets format.  Inline secrets must be an object")
-      }
-      // If the secrets object is empty, do nothing, else encrypt secrets
-      if (Object.keys(config.secrets).length !== 0) {
-        request.secrets =
-          "0x" +
-          (await (0, encryptSecrets_1.encryptWithSignature)(
-            config.walletPrivateKey,
-            config.DONPublicKey,
-            JSON.stringify(config.secrets)
-          ))
-      }
+    if (config.args) {
+        request.args = config.args;
     }
-    if (config.secretsLocation === getRequestConfig_1.Location_.Remote) {
-      if (!Array.isArray(config.secretsURLs)) {
-        throw Error("Unsupported remote secrets format.  Remote secrets must be an array.")
-      }
-      // If the secrets URLs is empty, do nothing, else encrypt secrets URLs
-      if (config.secretsURLs.length > 0) {
-        request.secrets =
-          "0x" + (await (0, encryptSecrets_1.encrypt)(config.DONPublicKey, config.secretsURLs.join(" ")))
-      }
-    }
-  }
-  if (config.args) {
-    request.args = config.args
-  }
-  return request
-}
-exports.buildRequest = buildRequest
+    return request;
+};
+exports.buildRequest = buildRequest;
