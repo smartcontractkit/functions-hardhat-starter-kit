d33e483939dd5537cda2117ec8594da3bb031d54
Zubin <zeuslawyer@users.noreply.github.com>
2023-09-14 08:45:43 +1000

    Readme Updates (#180)
    Co-authored-by: Morgan Kuphal <morgan.kuphal@smartcontract.com>

--- tasks/Functions-client/deployClient.js
+++ tasks/Functions-client/deployClient.js
@@ -12,8 +12,18 @@ task("functions-deploy-consumer", "Deploys the FunctionsConsumer contract")
     console.log("\n__Compiling Contracts__")
     await run("compile")
 
+    const overrides = {}
+    // If specified, use the gas price from the network config instead of Ethers estimated price
+    if (networks[network.name].gasPrice) {
+      overrides.gasPrice = networks[network.name].gasPrice
+    }
+    // If specified, use the nonce from the network config instead of automatically calculating it
+    if (networks[network.name].nonce) {
+      overrides.nonce = networks[network.name].nonce
+    }
+
     const consumerContractFactory = await ethers.getContractFactory("FunctionsConsumer")
-    const consumerContract = await consumerContractFactory.deploy(functionsRouter, donIdBytes32)
+    const consumerContract = await consumerContractFactory.deploy(functionsRouter, donIdBytes32, overrides)
 
     console.log(
       `\nWaiting ${networks[network.name].confirmations} blocks for transaction ${
