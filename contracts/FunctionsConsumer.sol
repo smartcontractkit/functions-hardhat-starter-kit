d3838dffa6eedcbd5414be18b3c71beccd0b914e
Zubin <zeuslawyer@users.noreply.github.com>
2023-09-27 05:35:07 +1000

    remove vendored contracts package (#184)
    Co-authored-by: Morgan Kuphal <morgan.kuphal@smartcontract.com>

--- contracts/FunctionsConsumer.sol
+++ contracts/FunctionsConsumer.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {FunctionsClient} from "./@chainlink/contracts/src/v0.8/functions/dev/1_0_0/FunctionsClient.sol";
-import {ConfirmedOwner} from "./@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
-import {FunctionsRequest} from "./@chainlink/contracts/src/v0.8/functions/dev/1_0_0/libraries/FunctionsRequest.sol";
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/libraries/FunctionsRequest.sol";
 
 /**
  * @title Chainlink Functions example on-demand consumer contract example
