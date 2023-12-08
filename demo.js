c1a82ce2fc46cb67c7f632dd0791cdbe03bb0b4d
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2023-12-08 16:05:56 -0600

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,21 +1,17 @@
-// Returns total number of NFTs a user holds in a given NFT collection
-
-// Uses latest version of Ethers (v6.9.0)
 const ethers = await import("npm:ethers")
 
-// Get the owner address to check and the address of the NFT contract from args
-const addressToCheck = args[0]
-const nftAddress = args[1]
-// Contract method ABI to get number of NFTs held by an address
-const abi = ["function balanceOf(address owner) view returns (uint256)"]
-// Chainlink Functions compatible Ethers JSON RPC provider class
-// (this is required for making Ethers RPC calls with Chainlink Functions)
+const userAddress = args[0]
+const nftContractAddress = args[1]
+const nftID = args[2]
+const abi = ["function ownerOf(uint256 tokenId) view returns (address)"]
+
+// Custom JsonRpcProvider class which using the fetch API
 class FunctionsJsonRpcProvider extends ethers.JsonRpcProvider {
   constructor(url) {
     super(url)
     this.url = url
   }
-  async _send(payload: ethers.JsonRpcPayload | Array<ethers.JsonRpcPayload>): Promise<Array<ethers.JsonRpcResult>> {
+  async _send(payload) {
     let resp = await fetch(this.url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -27,6 +23,10 @@ class FunctionsJsonRpcProvider extends ethers.JsonRpcProvider {
 
 const provider = new FunctionsJsonRpcProvider(secrets.RPC_URL)
 const nftContract = new ethers.Contract(nftAddress, abi, provider)
-const balance = await nftContract.balanceOf(addressToCheck)
+const ethNftOwner = await nftContract.ownerOf(addressToCheck)
 
-return Functions.encodeUint256(balance)
+if (ethNftOwner !== userAddress) {
+  throw new Error("User does not own NFT")
+}
+// Return to indicate success
+return new Uint8Array([1])
