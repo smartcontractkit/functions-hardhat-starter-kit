a1e153529c53ca969081ef3b72366e6ef0826206
aelmanaa <aelmanaa@gmail.com>
2024-01-09 12:17:33 +0100

    PRIVATE_KEY not needed for simulation

--- networks.js
+++ networks.js
@@ -12,13 +12,15 @@ const DEFAULT_VERIFICATION_BLOCK_CONFIRMATIONS = 2
 const npmCommand = process.env.npm_lifecycle_event
 const isTestEnvironment = npmCommand == "test" || npmCommand == "test:unit"
 
+const isSimulation = process.argv.length === 3 && process.argv[2] === "functions-simulate-script" ? true : false
+
 // Set EVM private keys (required)
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 // TODO @dev - set this to run the accept.js task.
 const SECOND_PRIVATE_KEY = process.env.SECOND_PRIVATE_KEY
 
-if (!isTestEnvironment && !PRIVATE_KEY) {
+if (!isTestEnvironment && !isSimulation && !PRIVATE_KEY) {
   throw Error("Set the PRIVATE_KEY environment variable with your EVM wallet private key")
 }
 
