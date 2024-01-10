9b6e8ad0f5012f471c13bc8989801747517b2180
Zubin Pratap <zubin.pratap@gmail.com>
2024-01-10 15:37:03 +1100

    Update for @chainlink/contracts@^1.0.0-beta.0
    Required changes to imports.
    
    NOTE: package.json not updated here.  Will need to be updated
    to point to ^1.0.0 when we go to RC.

--- contracts/test/MockV3Aggregator.sol
+++ contracts/test/MockV3Aggregator.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.7/tests/MockV3Aggregator.sol";
+import "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
