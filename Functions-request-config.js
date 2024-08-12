feba724865085fbea1ab70e37627cee0de7528e2
Justin Kaseman <justinkaseman@live.com>
2024-08-12 15:12:25 -0700

    Add tinygo HTTP from JS example

--- Functions-request-config.js
+++ Functions-request-config.js
@@ -4,7 +4,7 @@ const { Location, ReturnType, CodeLanguage } = require("@chainlink/functions-too
 // Configure the request by setting the fields below
 const requestConfig = {
   // String containing the source code to be executed
-  source: fs.readFileSync("./wasm-example-minimal.js").toString(),
+  source: fs.readFileSync("./wasm-example-tinygo-from-js.js").toString(),
   // Location of source code (only Inline is currently supported)
   codeLocation: Location.Inline,
   // Optional. Secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey). The secrets object can only contain string values.
@@ -12,7 +12,7 @@ const requestConfig = {
   // Optional if secrets are expected in the sourceLocation of secrets (only Remote or DONHosted is supported)
   secretsLocation: Location.Remote,
   // Args (string only array) can be accessed within the source code with `args[index]` (ie: args[0]).
-  args: ["2", "5"],
+  args: ["https://dummyjson.com/http/200/helloworld", "data.message"],
   // Code language (only JavaScript is currently supported)
   codeLanguage: CodeLanguage.JavaScript,
   // Expected type of the returned value
