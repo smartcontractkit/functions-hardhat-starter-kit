f35f7ed69934a053ba417e39e1b394b0361665fc
Morgan Kuphal <87319522+KuphJr@users.noreply.github.com>
2024-08-31 18:01:32 -0500

    Add Base & OP mainnet (#219)

--- networks.js
+++ networks.js
@@ -96,24 +96,6 @@ const networks = {
       "https://02.functions-gateway.testnet.chain.link/",
     ],
   },
-  polygonMumbai: {
-    url: process.env.POLYGON_MUMBAI_RPC_URL || "UNSET",
-    gasPrice: 20_000_000_000, // gas price for the functions request
-    nonce: undefined,
-    accounts,
-    verifyApiKey: process.env.POLYGONSCAN_API_KEY || "UNSET",
-    chainId: 80001,
-    confirmations: 10,
-    nativeCurrencySymbol: "MATIC",
-    linkToken: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
-    linkPriceFeed: "0x12162c3E810393dEC01362aBf156D7ecf6159528", // LINK/MATIC
-    functionsRouter: "0x6E2dc0F9DB014aE19888F539E59285D2Ea04244C",
-    donId: "fun-polygon-mumbai-1",
-    gatewayUrls: [
-      "https://01.functions-gateway.testnet.chain.link/",
-      "https://02.functions-gateway.testnet.chain.link/",
-    ],
-  },
   avalancheFuji: {
     url: process.env.AVALANCHE_FUJI_RPC_URL || "UNSET",
     gasPrice: undefined,
@@ -221,6 +203,36 @@ const networks = {
       "https://01.functions-gateway.testnet.chain.link/",
       "https://02.functions-gateway.testnet.chain.link/",
     ],
+  },
+  base: {
+    url: process.env.BASE_RPC_URL || "UNSET",
+    gasPrice: undefined,
+    nonce: undefined,
+    accounts,
+    verifyApiKey: process.env.BASESCAN_API_KEY || "UNSET",
+    chainId: 8453,
+    confirmations: DEFAULT_VERIFICATION_BLOCK_CONFIRMATIONS,
+    nativeCurrencySymbol: "ETH",
+    linkToken: "0x88Fb150BDc53A65fe94Dea0c9BA0a6dAf8C6e196",
+    linkPriceFeed: "0xc5E65227fe3385B88468F9A01600017cDC9F3A12", // LINK/ETH
+    functionsRouter: "0xf9b8fc078197181c841c296c876945aaa425b278",
+    donId: "fun-base-mainnet-1",
+    gatewayUrls: ["https://01.functions-gateway.chain.link/", "https://02.functions-gateway.chain.link/"],
+  },
+  optimism: {
+    url: process.env.OPTIMISM_RPC_URL || "UNSET",
+    gasPrice: undefined,
+    nonce: undefined,
+    accounts,
+    verifyApiKey: process.env.OPTISCAN_API_KEY || "UNSET",
+    chainId: 10,
+    confirmations: DEFAULT_VERIFICATION_BLOCK_CONFIRMATIONS,
+    nativeCurrencySymbol: "ETH",
+    linkToken: "0x350a791Bfc2C21F9Ed5d10980Dad2e2638ffa7f6",
+    linkPriceFeed: "0x464A1515ADc20de946f8d0DEB99cead8CEAE310d", // LINK/ETH
+    functionsRouter: "0xaA8AaA682C9eF150C0C8E96a8D60945BCB21faad",
+    donId: "fun-optimism-mainnet-1",
+    gatewayUrls: ["https://01.functions-gateway.chain.link/", "https://02.functions-gateway.chain.link/"],
   },
   // localFunctionsTestnet is updated dynamically by scripts/startLocalFunctionsTestnet.js so it should not be modified here
   localFunctionsTestnet: {
